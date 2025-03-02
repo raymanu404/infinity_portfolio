cbfc60293a548abb0fdb4bcbc01581b385048992
raymanu404 <manu.caprariu.brediceanu@gmail.com>
2025-03-02 02:18:26 +0200

    update

--- src/pages/Router/Components/RedirectRouter.tsx
+++ src/pages/Router/Components/RedirectRouter.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { PATH_ROUTES } from '@/pages/Router/constants'
 
--- src/pages/Router/index.tsx
+++ src/pages/Router/index.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from 'react'
+import { Suspense, lazy } from 'react'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { Layout } from '../Layout/index'
 import { PATH_ROUTES } from './constants'
