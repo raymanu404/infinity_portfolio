d7902e2fceaf1f46a42c453f15b6fe560e69fdd5
raymanu404 <manu.caprariu.brediceanu@gmail.com>
2025-04-25 17:18:34 +0300

    fix file formmater

--- src/pages/Projects/KnowledgeHub/Contents/ComponentsSection.tsx
+++ src/pages/Projects/KnowledgeHub/Contents/ComponentsSection.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ComponentsSection: React.FC = () => {
+  const [messageCount, setMessageCount] = useState(0);
+
+  console.log(messageCount);
+
   return (
     <div>
-      <h1>ComponentsSection</h1>
+      <h1>{messageCount > 0 && `Message Count ${messageCount}`}</h1>
+      <button
+        onClick={() => {
+          setMessageCount(prev => prev + 1);
+        }}
+      >
+        Increse Counter
+      </button>
     </div>
   );
 };
