2a51bfed58a88376a466f0a8210d766dcdd5bffc
raymanu404 <manu.caprariu.brediceanu@gmail.com>
2026-03-17 20:43:59 +0200

    finish useId

--- src/pages/Projects/KnowledgeHub/Contents/Tabs/pages/Hooks/UseId/UseIdContent.tsx
+++ src/pages/Projects/KnowledgeHub/Contents/Tabs/pages/Hooks/UseId/UseIdContent.tsx
@@ -1,11 +1,34 @@
+import { NoteI } from '@/Shared/Components/Notes/interfaces';
 import React from 'react';
 import { TemplatePageContent } from '../../components';
 import { USE_ID_HOOK_PAGE_CONTENTS } from '../constants';
 
 const PAGE_TITLE = 'UseId Hook';
+const PAGE_NOTES = [
+  {
+    note: 'Is a React Hook for generating unique IDs that can be passed to accessibility attributes',
+  },
+  {
+    note: ` should not be used to generate cache keys for use(). The ID is stable when a component is mounted but may change during rendering. Cache keys should be generated from your data.`,
+  },
+  {
+    note: `should not be used to generate keys in a list`,
+  },
+  {
+    note: `currently cannot be used in async Server Components.`,
+  },
+] as NoteI[];
 
 const UseIdContent: React.FC = () => {
-  return <TemplatePageContent pageTitle={PAGE_TITLE} pageContentList={USE_ID_HOOK_PAGE_CONTENTS} />;
+  return (
+    <TemplatePageContent
+      pageTitle={PAGE_TITLE}
+      pageContentList={USE_ID_HOOK_PAGE_CONTENTS}
+      notes={{
+        notes: PAGE_NOTES,
+      }}
+    />
+  );
 };
 
 UseIdContent.displayName = 'UseIdContent';
